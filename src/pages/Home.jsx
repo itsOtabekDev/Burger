f6a60375fc70553cf3a54e3f5c870f90cf0fdcf3
Otabek <atabekabdusharipov@gmail.com>
2023-03-07 17:58:10 +0200

    asd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import Header from "../components/Header"
 import Navbar from "../components/Navbar"
 import Rounded from "../components/rounded"
 import Section from "../components/section"
+import Wrapper from "../components/wrapper"
 
 
 const Home = () => {
