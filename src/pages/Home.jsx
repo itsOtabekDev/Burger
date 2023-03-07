8b918f3a191d60c73b91db5facbcb6895a9ea796
Otabek <atabekabdusharipov@gmail.com>
2023-03-07 17:47:43 +0200

    asd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import Burger from '../components/Burger'
 import Card from '../components/card'
 import Cards from '../components/cards'
-import Footer from '../components/footer'
-import Header from '../components/header'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
 import Navbar from '../components/Navbar'
 import Rounded from '../components/rounded'
 import Section from '../components/section'
-import Wrapper from '../components/wrapper'
+import Wrapper from '../components/Wrapper'
 
 const Home = () => {
     return (
